c4d6201060eb79ee7d2bf3c258b35cd6ab1328e1
shoalam <shoalamdu@gmail.com>
2020-09-23 01:02:23 +0600

    First Commit

---
+++ react-feed/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+     render() {
+        return (
+            <div className="row" id="footer">
+                <div className="medium-12 columns">
+                <p>Copyright &copy; 2020 </p>
+                </div>
+            </div>
+       );
+   }
+}
+export default Footer;
